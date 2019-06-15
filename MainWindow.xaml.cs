6250a3213c9ab0fab8eadd145b8cace0b01029f8
Teun Wiermsa <Teun.Wiersma@student.hu.nl>
2019-06-15 16:17:23 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Store
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        string username;
+        string password;
+
+        private void Login(object sender, RoutedEventArgs e)
+        {
+            //inlog functie hiero
+            username = Name.Text;
+            password = Password.Text;
+            // test voor tweede window shit
+            if (username == "admin" && password == "admin")
+            {
+                SecondWindow secondWindow = new SecondWindow();
+                this.Visibility = Visibility.Hidden;
+                secondWindow.Show();
+            }
+            
+        }
+
+        private void Register(object sender, RoutedEventArgs e)
+        {
+            // register functie hiero
+        }
+    }
+}
---
+++ SecondWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Store
+{
+    /// <summary>
+    /// Interaction logic for SecondWindow.xaml
+    /// </summary>
+    public partial class SecondWindow : Window
+    {
+        public SecondWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // koop button
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            // Opnieuw ophalen van voorraad en producten
+        }
+    }
+}
